675efc3326b148e6a6456dd0ed58803bf231e6b7
Janxxy <lesley.pond@gmail.com>
2019-01-21 00:57:43 -0500

    remove favorites button

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -1,14 +1,23 @@
 $("#gifs").on("click","button", function(){
 var dataValue = $(this).attr("data-value");
 console.log(dataValue);
+var newDiv=$("<div>");
+newDiv.css({float:"left"});
 var imageToAppend = $("#image" + dataValue).clone();
-$("#favoritesHere").append(imageToAppend);
-
+imageToAppend.css({margin:"10px"});
+newDiv.append(imageToAppend);
+removeButton = $("<button>");
+removeButton.addClass("btn btn-danger");
+removeButton.text("Remove from favorites");
+removeButton.css({margin:"10px", fontSize:"10px"});
+newDiv.append(removeButton);
+$("#favoritesHere").append(newDiv);
 });
 
 $("#favoritesLink").on("click",function(){
 window.location="#favorites";
 })
+
 $("#favoritesHere").on("click","img", function(){
     var state= $(this).attr("data-state");
     if (state === "still") {
@@ -19,3 +28,7 @@ $("#favoritesHere").on("click","img", function(){
      $(this).attr("data-state", "still");
    }
   });
+$("#favoritesHere").on("click","button", function(){
+  var remove=$(this).parent();
+  remove.remove();
+  });  
